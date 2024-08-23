e8d0f9b6be079909bfba3c6ebc7b347fe4ee61e7
xh ne <ntxhuong04@gmail.com>
2024-08-23 17:30:30 +0700

    update

--- src/services/chatbotService.js
+++ src/services/chatbotService.js
@@ -27,7 +27,7 @@ let callSendAPI = (sender_psid, response) => {
     });
 }
 
-let handleGetStarted = () => {
+let handleGetStarted = (sender_psid) => {
     return Promise(async (resolve, reject) => {
         try{
             let response = { "text": "Chào mừng bạn đến với wse enroll-bot - hệ thống tư vấn tuyển sinh tự động ĐHQG-HCM."}
